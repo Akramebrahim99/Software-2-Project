6859b7cf5d3462f5785f4d21bf9add5a199bb832
Akramebrahim99 <akramebrahim99@yahoo.com>
2020-05-23 15:25:05 +0200

    Create admin layout

--- PMS/PMS/App_Start/RouteConfig.cs
+++ PMS/PMS/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace PMS
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Admin", action = "HomePage", id = UrlParameter.Optional }
             );
         }
     }

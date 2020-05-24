69cf9525bfbecb44e911098fd98c2a67f5d0bea1
Engineer-Hossam <hossan.h.hassan@gmail.com>
2020-05-24 19:07:09 +0200

    Admin Login

--- PMS/PMS/App_Start/RouteConfig.cs
+++ PMS/PMS/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace PMS
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Admin", action = "HomePage", id = UrlParameter.Optional }
+                defaults: new { controller = "Account", action = "Login", id = UrlParameter.Optional }
             );
         }
     }
---
+++ PMS/PMS/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using PMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PMS.Controllers
+{
+    public class AccountController : Controller
+    {
+        private Pharmacy db = new Pharmacy();
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Admin admin)
+        {
+            if (admin.Admin_Name == "Admin" && admin.Password == "Admin")
+            {
+                return RedirectToAction("HomePage", "Admin");
+            }
+            ViewBag.Error = "*Invalid User Name Or Password";
+            return View(admin);
+        }
+    }
+}

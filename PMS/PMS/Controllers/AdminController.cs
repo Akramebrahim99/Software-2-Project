1c192d6771a9d011067502b4529272a0ca3a8405
eslamamged74 <solomamged74@gmail.com>
2020-05-29 09:02:37 +0200

    Admin  View All Product & Add Product

--- PMS/PMS/Controllers/AdminController.cs
+++ PMS/PMS/Controllers/AdminController.cs
@@ -1,6 +1,7 @@
 ﻿using PMS.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -15,6 +16,51 @@ namespace PMS.Controllers
         public ActionResult AllClient()
         {
             return View(db.Clients.ToList());
+        }
+
+
+        public ActionResult AddProduct()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Addproduct(item item)
+        {
+            if (item.ImageFile == null)
+            {
+                ViewBag.error = "*Required";
+            }
+            else
+            {
+                string FileName = Path.GetFileNameWithoutExtension(item.ImageFile.FileName);
+                string Extention = Path.GetExtension(item.ImageFile.FileName);
+                FileName = FileName + DateTime.Now.ToString("yymmssfff") + Extention;
+                item.Image = "~/Image/" + FileName;
+                FileName = Path.Combine(Server.MapPath("~/Image/"), FileName);
+
+
+                if (Extention.ToLower() == ".jpg" || Extention.ToLower() == ".jpeg" || Extention.ToLower() == ".png")
+                {
+                    if (ModelState.IsValid)
+                    {
+                        item.ImageFile.SaveAs(FileName);
+                        db.items.Add(item);
+                        db.SaveChanges();
+                        return RedirectToAction("AllProduct");
+                    }
+                }
+                else
+                {
+                    ViewBag.msg = "Invaild File Type";
+                }
+
+            }
+            return View(item);
+        }
+
+        public ActionResult AllProduct()
+        {
+            return View(db.items.ToList());
         }
 
     }

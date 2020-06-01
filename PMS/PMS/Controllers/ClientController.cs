cf252cc6f8895fd5ec11421023da2120f27f59c7
Engineer-Hossam <hossan.h.hassan@gmail.com>
2020-06-01 10:27:48 +0200

    Client Create Order

--- PMS/PMS/Controllers/ClientController.cs
+++ PMS/PMS/Controllers/ClientController.cs
@@ -12,6 +12,8 @@ namespace PMS.Controllers
     public class ClientController : Controller
     {
         private PharmacyEntities db = new PharmacyEntities();
+
+        ////////////////////////////////////// Home page and profile  //////////////////////////////////
         public ActionResult HomePage(int? id)
         {
             if (id == null)
@@ -37,7 +39,9 @@ namespace PMS.Controllers
             }
             return View(client);
         }
+        /////////////////////////////////////////////////////////////////////////////////////////////
 
+        ////////////////////////////////////// Edit Profile  ////////////////////////////////////////
         public ActionResult EditProfile()
         {
 
@@ -75,5 +79,71 @@ namespace PMS.Controllers
             }
             return View(client);
         }
+        /////////////////////////////////////////////////////////////////////////////////////////////
+
+        ////////////////////////////////////// Create Order   //////////////////////////////////
+        public ActionResult CreateOrder()
+        {
+            ViewBag.Item_id = new SelectList(db.items, "Id", "Name");
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult CreateOrder(Order order)
+        {
+            order.Client_id = (int)Session["BranchId"];
+            var item = db.items.Find(order.Item_id);
+            var ordercheck = (from ClientList in db.Orders
+                              where ClientList.Item_id == order.Item_id && ClientList.Client_id == order.Client_id
+                              select new
+                              {
+                                  ClientList.Id
+                              });
+
+
+            if (ordercheck.FirstOrDefault() == null)
+            {
+                if (item.Quentity >= order.Quentity)
+                {
+                    if (item.Discount.Equals(null))
+                    {
+                        order.Total_Price = item.Price * order.Quentity;
+                    }
+                    else
+                        order.Total_Price = (item.Price * order.Quentity) - ((double)item.Discount * order.Quentity);
+                    item.Quentity = item.Quentity - order.Quentity;
+                    if (ModelState.IsValid)
+                    {
+                        db.Entry(item).State = EntityState.Modified;
+                        db.Orders.Add(order);
+                        db.SaveChanges();
+                        return RedirectToAction("MyProfile");
+                    }
+                    ViewBag.Client_id = new SelectList(db.Clients, "Id", "Name", order.Client_id);
+                    ViewBag.Item_id = new SelectList(db.items, "Id", "Name", order.Item_id);
+                    return View(order);
+                }
+                else
+                {
+                    ViewBag.Error = "Max Quentity = " + item.Quentity.ToString();
+                    ViewBag.Client_id = new SelectList(db.Clients, "Id", "Name", order.Client_id);
+                    ViewBag.Item_id = new SelectList(db.items, "Id", "Name", order.Item_id);
+                    return View(order);
+                }
+
+            }
+            else
+            {
+                ViewBag.msg = "This client has this product in his order";
+                ViewBag.Client_id = new SelectList(db.Clients, "Id", "Name", order.Client_id);
+                ViewBag.Item_id = new SelectList(db.items, "Id", "Name", order.Item_id);
+                return View(order);
+            }
+
+
+        }
+        /////////////////////////////////////////////////////////////////////////////////////////
+
     }
 }
